05f31d513e4854413e3a8e261bda67c5335d9874
jjessieshang <17jessies@gmail.com>
2023-02-01 23:35:25 -0800

    Initial Commit

---
+++ espensive/espensiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  espensiveApp.swift
+//  espensive
+//
+//  Created by Jessie Shang on 2023-02-01.
+//
+
+import SwiftUI
+
+@main
+struct espensiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
